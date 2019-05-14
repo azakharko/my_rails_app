74d61425aa44a9db8687644051eae9c54e9fc6ab
Andriy Zakharko <azakharko@gmail.com>
2019-05-14 11:50:55 -0500

    work in progress - add some comments stuff

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -6,23 +6,43 @@ class PostsController < ApplicationController
     @posts = Post.all
   end
 
-  def new
-    @post = Post.new
-  end
-
   def show
     @post = Post.find(params[:id])
   end
 
+  def new
+    @post = Post.new
+  end
+
   def create
-    # render plain: params[:post].inspect
     @post = Post.new(post_params)
 
-    if(@post.save)
+    if @post.save
       redirect_to @post
     else
       render 'posts/new'
     end
+  end
+
+  def edit
+    @post = Post.find(params[:id])
+  end
+
+  def update
+    @post = Post.find(params[:id])
+
+    if @post.update(post_params)
+      redirect_to @post
+    else
+      render 'posts/edit'
+    end
+  end
+
+  def destroy
+    @post = Post.find(params[:id])
+    @post.destroy
+
+    redirect_to posts_path
   end
 
   private
